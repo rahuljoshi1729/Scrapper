39e0adc54fb3965bca65bf0cb068d5eca69173ff
rahuljoshi1729 <rahul22100@akgec.ac.in>
2024-10-18 15:25:55 +0530

    update: codeforces serializer

--- scrapper/codeforces/serializers.py
+++ scrapper/codeforces/serializers.py
@@ -1,44 +1,8 @@
 from rest_framework import serializers
 from datetime import datetime
 
-
-class userdataserializer(serializers.Serializer):
-    firstName = serializers.CharField()
-    lastName = serializers.CharField()
-    country = serializers.CharField()
-    city = serializers.CharField()
-    rating = serializers.IntegerField()
-    maxRating = serializers.IntegerField()
-    rank = serializers.CharField()
-    maxRank = serializers.CharField()
-    contribution = serializers.IntegerField()
-    handle = serializers.CharField()
-    friendOfCount = serializers.IntegerField()
-    titlePhoto = serializers.CharField()
-    avatar = serializers.CharField()
-    organization = serializers.CharField()
-    registrationTime = serializers.SerializerMethodField()
-    lastOnlineTime = serializers.SerializerMethodField()
-    
-    def registrationTime(self, obj):
-        return datetime.fromtimestamp(obj['registrationTimeSeconds']).strftime('%Y-%m-%d %H:%M:%S')
-
-    def lastOnlineTime(self, obj):
-        return datetime.fromtimestamp(obj['lastOnlineTimeSeconds']).strftime('%Y-%m-%d %H:%M:%S')
-
 class submissionserializer(serializers.Serializer):
     contestId = serializers.IntegerField()
     creationTimeSeconds = serializers.CharField()
     
 
-class ratingserializer(serializers.Serializer):
-    contestId = serializers.IntegerField()
-    contestName = serializers.CharField()
-    handle = serializers.CharField()
-    rank = serializers.IntegerField()
-    oldRating = serializers.IntegerField()
-    newRating = serializers.IntegerField()
-    ratingUpdateTime = serializers.SerializerMethodField()
-    
-    def ratingUpdateTime(self, obj):
-        return datetime.fromtimestamp(obj['ratingUpdateTimeSeconds']).strftime('%Y-%m-%d %H:%M:%S')
